5b8af44cca23833b7deca3fd066796aa0c927cb0
Monster <mahyar.etedal@icloud.com>
2019-12-22 19:28:52 -0500

    add bash file

--- automate.sh
+++ automate.sh
@@ -1,1 +1,1 @@
-python automate.py --relay1_on 19 --relay1_off 23 --relay2_on 23 --relay2_off 24 --relay3_on 24 --relay3_off 25 --relay4_on 25 --relay4_off 26
+python /home/debian/MiniAutomation/automate.py --relay1_on 19 --relay1_off 23 --relay2_on 23 --relay2_off 24 --relay3_on 24 --relay3_off 25 --relay4_on 25 --relay4_off 26
